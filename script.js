953cf09f7adebd8c21034230e28c464f2dd797ca
awsafAli <37135943+awsafAli@users.noreply.github.com>
2021-10-03 16:48:09 +0530

    final chnages
    final chnages

--- script.js
+++ script.js
@@ -5,14 +5,12 @@ const fetchPokemon = async () => {
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=50`); 
    var results = await res.json(); 
   
-   // console.log(results);
+  
   results.results.map((result) => {
     
     let obj = {
     name: result.name,
-   // image: result.sprites['front_default'],
-   //     type: result.types.map((type) => type.type.name).join(', '),
-   // id: result.id
+  
    };
   
   fetchInnerData(result.url,obj);
@@ -25,6 +23,7 @@ catch(err){
 }  
 };
 fetchPokemon();
+
 var card = document.createElement("div");
 card.className = "container-fluid row";  
 
@@ -32,16 +31,19 @@ card.className = "container-fluid row";
 const fetchInnerData = async (url, obj) => {
   const res =  await fetch(url); 
   var results = await res.json(); 
- //console.log( 'resulrt',results)
+ 
  
     obj['image'] =  results.sprites['front_default'];
     obj['type'] = results.types.map((type) => type.type.name).join(', ');
     obj['id'] = results.id;
     obj['weight'] = results.weight;
-    obj['moves'] = results.moves;
-    obj['abilities'] = results.abilities;
+    //obj['moves'] = results.moves;
+
+    obj['moves'] = results.moves.map((moves) => moves.move.name).join(', ');
+    obj['abilities'] = results.abilities.map((abilities) => abilities.ability.name).join(', ');
+   // obj['abilities'] = results.abilities;
     pokemon.push(obj);
-    console.log('poke',pokemon);
+    console.log('poke', results.types.map((type) => type.type.name).join(', '));
          
 
   
@@ -50,27 +52,47 @@ for (var i = 0; i < pokemon.length; i++)
 {
 
   var table  = `
-  <div class="col-sm-3 col-md-6 col-lg-3 col-xl-3 col-xs-12">
+  <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xs-12">
   <div class="inner">
     <div class="row">
       <div class="col-md-12 pokemon">
-          
-          <div class="img-container">
-          <img src="${pokemon[i].image}" class="imgtag">
-        </div>
+           <div class="row">
+                <div class="col-md-3 pokemon-image">
+
+                  <div class="img-container">
+                    <img src="${pokemon[i].image}" class="imgtag">
+                  </div>
+                </div>
+
+
+                <div class="col-md-9">
+                <div class="container">
+                  <p><b>Name</b>:<span>${pokemon[i].name}</span></p>
+                  <p><b>Abilities</b>: <span>${pokemon[i].abilities} </span></p>
+                  <p><b>Weight</b>: <span>${pokemon[i].weight} </span></p>
+                </div>  
+                </div>
+            </div>    
+
+         
       </div>
+
+
+
+
     
       <div class="col-md-12">
         <div class="container">
       
-          <p><b>Name</b>:<span>${pokemon[i].name}</span></p>
-          <p><b>Moves</b>:<span>
 
-                             ${pokemon[i].moves[0].move.name} 
-                             </span>
-                          </p>
-          <p><b>Abilities</b>: <span>${pokemon[i].abilities[0].ability.name} </span></p>
-          <p><b>Weight</b>: <span>${pokemon[i].weight} </span></p>
+          <p><b>Moves</b>:<span>
+                            
+          ${pokemon[i].moves} 
+          </span>
+          </p>
+         
+                    
+         
         </div>
       </div>
     </div>
