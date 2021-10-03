2de9bae687aa25be2f626be6ee46cadb0959165a
awsafAli <37135943+awsafAli@users.noreply.github.com>
2021-10-03 14:03:06 +0530

    Updated code

--- script.js
+++ script.js
@@ -25,6 +25,9 @@ catch(err){
 }  
 };
 fetchPokemon();
+var card = document.createElement("div");
+card.className = "container-fluid row";  
+
 
 const fetchInnerData = async (url, obj) => {
   const res =  await fetch(url); 
@@ -41,41 +44,37 @@ const fetchInnerData = async (url, obj) => {
     console.log('poke',pokemon);
          
 
-    const card = document.createElement("div");
-    card.className = "container-fluid row";  
+  
     
 for (var i = 0; i < pokemon.length; i++) 
 {
 
-  card.innerHTML = `
-  
-  
-
+  var table  = `
   <div class="col-sm-3 col-md-6 col-lg-3 col-xl-3 col-xs-12">
-										<div class="inner">
-											<div class="row">
-												<div class="col-md-12">
-													<img src="${pokemon[i].image}" class="imgtag">
-												</div>
-									
-												<div class="col-md-12">
-													<div class="container">
-													
-                            <p><b>Name</b>: ${pokemon[i].name}</p>
-														<p><b>Moves</b>: ${pokemon[i].moves[0].move.name}</p>
-														<p><b>Abilities</b>: ${pokemon[i].abilities[0].ability.name}</p>
-														<p><b>Weight</b>:${pokemon[i].weight}</p>
-                            </div>
-												</div>
-										</div>
-									</div>
-								</div>
-
+  <div class="inner">
+    <div class="row">
+      <div class="col-md-12">
+      <img src="${pokemon[i].image}" class="imgtag">
+      </div>
+    
+      <div class="col-md-12">
+        <div class="container">
+       
+          <p><b>Name</b>:${pokemon[i].name}</p>
+          <p><b>Moves</b>:${pokemon[i].moves[0].move.name}</p>
+          <p><b>Abilities</b>:${pokemon[i].abilities[0].ability.name}</p>
+          <p><b>Weight</b>:${pokemon[i].weight}</p>
+        </div>
+      </div>
+    </div>
+    </div>
+</div>
 `
-             
+          
 }
-
+card.innerHTML += table;  
 document.body.append(card);  
+
 }
 
 
